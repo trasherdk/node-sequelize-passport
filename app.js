013c6daf6db8dcd5786f88c8bb53036f41a7783c
trasherdk <trasherdk@fumlersoft.dk>
2019-04-08 10:18:10 +0700

    Changing to pug veiw engine.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
