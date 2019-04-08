f287352e5ea2c23e0cf47546ea1da467d96c1659
trasherdk <trasherdk@fumlersoft.dk>
2019-04-08 13:29:19 +0700

    Change default mode to development

--- app.js
+++ app.js
@@ -67,26 +67,27 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') !== 'development'){}
+// production error handler
+// no stacktraces leaked to user
+	app.use(function(err, req, res, next) {
+	    res.status(err.status || 500);
+	    res.render('error', {
+	        message: err.message,
+	        error: {}
+	    });
+	});
+else {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             user: req.user,
             message: err.message,
-            error: err
+            errors: err
         });
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 
 module.exports = app;
