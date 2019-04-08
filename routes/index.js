14ad4d5b41a7e3f353aa00eac2a8e3a01ec959d2
trasherdk <trasherdk@fumlersoft.dk>
2019-04-08 12:39:17 +0700

    Start changing views to pug.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { user: req.user, message: null });
+  res.render('home', { user: req.user, message: null });
 });
 
 module.exports = router;
