6089eae767203ec3a137835511185585fa203765
TrasherDK <trasherdk@fumlersoft.dk>
2019-04-17 15:06:04 +0700

    App restructure

--- server.js
+++ server.js
@@ -11,3 +11,12 @@ const join = require('path').join;
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
+
+const config = require('./config');
+const models = join(__dirname, 'models');
+
+// Bootstrap models
+fs
+  .readdirSync(models)
+  .filter(file => ~file.indexOf('.js'))
+  .forEach(file => require(join(models, file)));
